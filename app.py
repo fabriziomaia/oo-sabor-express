b5141d99114337f452e12da275ff251cc31bf2e7
Fabrizio <fabrizio.apparicio@gmail.com>
2025-05-13 01:47:37 -0300

    Code Update v7 - herança

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 5)
-restaurante_praca.receber_avaliacao('Lais', 4)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+# restaurante_praca.receber_avaliacao('Gui', 5)
+# restaurante_praca.receber_avaliacao('Lais', 4)
+# restaurante_praca.receber_avaliacao('Emy', 2)
 
 # restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 # restaurante_japones = Restaurante('Japa', 'Japonesa')
@@ -11,7 +15,9 @@ restaurante_praca.receber_avaliacao('Emy', 2)
 # restaurante_mexicano.alternar_estado()
 
 def main():
-    Restaurante.listar_restaurantes()
+    # Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 
 if __name__ == '__main__':
